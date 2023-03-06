6df9906dda47c4754b77dcedd5108345741fc744
nevepura <federico.beach@gmail.com>
2023-03-06 23:31:38 +0100

    First commit: the example works

---
+++ src/main/java/com/example/GraphQLServer/controllers/BookController.java
@@ -0,0 +1,23 @@
+package com.example.GraphQLServer.controllers;
+
+import com.example.GraphQLServer.model.Author;
+import com.example.GraphQLServer.model.Book;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.getAuthorId());
+    }
+
+
+}
---
+++ src/test/java/com/example/GraphQLServer/GraphQlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GraphQLServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
